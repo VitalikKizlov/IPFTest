5e1d07e9b0616c1eb2b5cdbbad85a008a0a695c3
Vitalii Kizlov <vitalii kizlov>
2024-01-03 22:02:25 +0100

    Initial Commit

---
+++ IPFTest/IPFTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  IPFTestApp.swift
+//  IPFTest
+//
+//  Created by Vitalii Kizlov on 03.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct IPFTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
